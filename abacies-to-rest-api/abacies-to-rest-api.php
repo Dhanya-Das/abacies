9c29625285133230fa53261eddd4707117d7aa12
unknown <dhanya@abacies.com>
2023-05-08 09:06:40 +0530

    update

--- abacies-to-rest-api/abacies-to-rest-api.php
+++ abacies-to-rest-api/abacies-to-rest-api.php
@@ -18,7 +18,8 @@ define( 'ABACIES_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 include(ABACIES_PLUGIN_DIR.'/api/listing-api.php');
 
 /*
-*API for Home View
+*API for All list View
+http://localhost/abacies/wp-json/view/v1/all-pages
 */
 function get_all_postType() {
   $pt_ary = array (
@@ -31,18 +32,36 @@ function get_all_postType() {
 }
 
 add_action( 'rest_api_init', function () {
-  register_rest_route( 'view/v1', '/all-post', array(
+  register_rest_route( 'view/v1', '/all-pages', array(
     'methods' => 'GET',
     'callback' => 'get_all_postType',
   ) );
 } );
+/*
+*API for Page Single view
+*/
+function get_single_page_postType($request) {
+  // $id = 14;
+  $id =$request['id'];
+  $page = get_post($id);
+  $page_details = $page->post_content;
+  return $page_details;
+}
+  
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'view/v1', '/view-page/(?P<id>\d+)', array(
+  'methods' => 'GET',
+    'callback' => 'get_single_page_postType',
+  ) );
+} );
+
 
 /*
-*API for Servies View
+*API for Servies list View
 */
 function get_servies_postType() {
   $ser_ary = array (
-    'post_type' => 'servies',
+    'post_type' => 'services',
     'posts_per_page' => -1,
     'post_status' =>'publish',
     'order'=> 'ASC');
@@ -51,14 +70,14 @@ function get_servies_postType() {
 }
   
 add_action( 'rest_api_init', function () {
-  register_rest_route( 'view/v1', '/servies', array(
+  register_rest_route( 'view/v1', '/services', array(
     'methods' => 'GET',
     'callback' => 'get_servies_postType',
   ) );
 } );
 
 /*
-*API for Solutions View
+*API for Solutions list View
 */
 function get_solutions_postType() {
   $sol_ary = array (
@@ -78,7 +97,7 @@ add_action( 'rest_api_init', function () {
 } );
 
 /*
-*API for Products View
+*API for Products list View
 */
 function get_products_postType() {
   $pdt_ary = array (
@@ -91,9 +110,15 @@ function get_products_postType() {
 }
   
 add_action( 'rest_api_init', function () {
-  register_rest_route( 'view/v1', '/solutions', array(
+  register_rest_route( 'view/v1', '/products', array(
     'methods' => 'GET',
     'callback' => 'get_products_postType',
   ) );
 } );
+
+add_action('init', 'init_test');
+function init_test(){
+  echo "hello";
+
+}
 
