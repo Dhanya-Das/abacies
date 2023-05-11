946c25c97184a38c570173a82ce6fabe0d59c237
unknown <dhanya@abacies.com>
2023-05-11 08:11:43 +0530

    update

--- abacies-to-rest-api/abacies-to-rest-api.php
+++ abacies-to-rest-api/abacies-to-rest-api.php
@@ -21,74 +21,151 @@ include(ABACIES_PLUGIN_DIR.'/api/listing-api.php');
 *API for All list View
 http://localhost/abacies/wp-json/view/v1/all-pages
 */
-function get_all_postType() {
-  $pt_ary = array (
-    'post_type' => 'page',
-    'posts_per_page' => -1,
-    'post_status' =>'publish',
-    'order'=> 'ASC');
-    $post_ary = get_posts($pt_ary);
-    return $post_ary;
+
+/*
+*API for Header Menus
+*/
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'view/v1', '/header-menu', array(
+      'methods' => 'GET',
+      'callback' => 'get_header_menus',
+  ) );
+} );
+
+function get_header_menus() {
+  $menu_name = 'header-menu';
+  $menu = wp_get_nav_menu_object( $menu_name );
+  $menu_items = wp_get_nav_menu_items( $menu->term_id );
+
+  $menu_array = array();
+  foreach ( (array) $menu_items as $key => $menu_item ) {
+    $menu_array[] = array(
+      'id' => $menu_item->ID,
+      'title' => $menu_item->title,
+      'url' => $menu_item->url,
+      'children' => get_menu_item_children( $menu_items, $menu_item->ID ),
+    );
+  }
+  if( empty($menu_array) ){
+    return new WP_Error( 'no_header', __('No post found'), array( 'status' => 404 ) );
+  }else {
+    return new WP_REST_Response( $menu_array, 200 );
+  }
 }
 
+function get_menu_item_children( $menu_items, $parent_id ) {
+  $children = array();
+  foreach ( (array) $menu_items as $menu_item ) {
+    if ( $menu_item->menu_item_parent == $parent_id ) {
+      $children[] = array(
+        'id' => $menu_item->ID,
+        'title' => $menu_item->title,
+        'url' => $menu_item->url,
+        'children' => get_menu_item_children( $menu_items, $menu_item->ID ),
+      );
+    }
+  }
+  return $children;
+}
+
+/*
+*API for Page Single view
+*/
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'view/v1', '/view-page/(?P<slug>[a-zA-Z0-9-]+)', array(
+    'methods' => 'GET',
+    'callback' => 'get_single_page_postType',
+  ) );
+
+} );
+
+function get_single_page_postType($request) {
+  $params = $request->get_params();
+  $slug = $params['slug'];
+
+  $page = get_page_by_path( $slug );
+  if ( ! $page ) {
+    return new WP_Error( '404', 'Page not found', array( 'status' => 404 ) );
+  }
+
+  $page_data = array(
+    'id' => $page->ID,
+    'title' => $page->post_title,
+    'content' => apply_filters( 'the_content', $page->post_content ),
+  );
+
+  return new WP_REST_Response( $page_data, 200 );
+}
+
+
+/*
+*API for All Page view
+*/
 add_action( 'rest_api_init', function () {
   register_rest_route( 'view/v1', '/all-pages', array(
     'methods' => 'GET',
     'callback' => 'get_all_postType',
   ) );
 } );
-/*
-*API for Page Single view
-*/
-function get_single_page_postType($request) {
-  // $id = 14;
-  $id =$request['id'];
-  $page = get_post($id);
-  $page_details = $page->post_content;
-  return $page_details;
+
+function get_all_postType($request) {
+  $pages = get_pages();
+
+  if ( empty( $pages ) ) {
+    return new WP_Error( '404', 'No pages found', array( 'status' => 404 ) );
+  }
+
+  $page_data = array();
+  foreach ( $pages as $page ) {
+    $page_data[] = array(
+      'id' => $page->ID,
+      'title' => $page->post_title,
+      'content' => apply_filters( 'the_content', $page->post_content ),
+    );
+  }
+
+  return new WP_REST_Response( $page_data, 200 );
 }
-  
-add_action( 'rest_api_init', function () {
-  register_rest_route( 'view/v1', '/view-page/(?P<id>\d+)', array(
-  'methods' => 'GET',
-    'callback' => 'get_single_page_postType',
-  ) );
-} );
+
 
 
 /*
 *API for Servies list View
 */
-function get_servies_postType() {
-  $ser_ary = array (
-    'post_type' => 'services',
-    'posts_per_page' => -1,
-    'post_status' =>'publish',
-    'order'=> 'ASC');
-    $servies_ary = get_posts($ser_ary);
-  return $servies_ary;
-}
-  
 add_action( 'rest_api_init', function () {
   register_rest_route( 'view/v1', '/services', array(
     'methods' => 'GET',
     'callback' => 'get_servies_postType',
   ) );
 } );
+function get_servies_postType($request) {
+  $custom_post_type = get_posts( array(
+    'post_type' => 'services',
+    'posts_per_page' => -1,
+    'post_status' =>'publish',
+    'order'=> 'ASC'
+  ) );
+  if ( empty( $custom_post_type ) ) {
+    return new WP_Error( '404', 'No custom post type found', array( 'status' => 404 ) );
+  }
+
+  $custom_post_type_data = array();
+  foreach ( $custom_post_type as $post ) {
+    $custom_post_type_data[] = array(
+      'id' => $post->ID,
+      'title' => $post->post_title,
+      'content' => apply_filters( 'the_content', $post->post_content ),
+    );
+  }
+
+  return new WP_REST_Response( $custom_post_type_data, 200 );
+
+}
+  
 
 /*
 *API for Solutions list View
 */
-function get_solutions_postType() {
-  $sol_ary = array (
-    'post_type' => 'solutions',
-    'posts_per_page' => -1,
-    'post_status' =>'publish',
-    'order'=> 'ASC');
-    $solutions_ary = get_posts($sol_ary);
-  return $solutions_ary;
-}
-  
 add_action( 'rest_api_init', function () {
   register_rest_route( 'view/v1', '/solutions', array(
     'methods' => 'GET',
@@ -96,18 +173,33 @@ add_action( 'rest_api_init', function () {
   ) );
 } );
 
+function get_solutions_postType($request) {
+  $custom_post_type = get_posts( array(
+    'post_type' => 'solutions',
+    'posts_per_page' => -1,
+    'post_status' =>'publish',
+    'order'=> 'ASC'
+  ) );
+  if ( empty( $custom_post_type ) ) {
+    return new WP_Error( '404', 'No custom post type found', array( 'status' => 404 ) );
+  }
+
+  $custom_post_type_data = array();
+  foreach ( $custom_post_type as $post ) {
+    $custom_post_type_data[] = array(
+      'id' => $post->ID,
+      'title' => $post->post_title,
+      'content' => apply_filters( 'the_content', $post->post_content ),
+    );
+  }
+
+  return new WP_REST_Response( $custom_post_type_data, 200 );
+}
+  
+
 /*
 *API for Products list View
 */
-function get_products_postType() {
-  $pdt_ary = array (
-    'post_type' => 'products',
-    'posts_per_page' => -1,
-    'post_status' =>'publish',
-    'order'=> 'ASC');
-    $products_ary = get_posts($pdt_ary);
-  return $products_ary;
-}
   
 add_action( 'rest_api_init', function () {
   register_rest_route( 'view/v1', '/products', array(
@@ -115,54 +207,32 @@ add_action( 'rest_api_init', function () {
     'callback' => 'get_products_postType',
   ) );
 } );
-/*
-*API for Header Menus
-*/
-function get_header_menu_list() {
-  $menuLocations = get_nav_menu_locations();
-  $menuID = $menuLocations['primary'];
-  $menus = [];
+function get_products_postType($request) {
   
-  $primaryNav = wp_get_nav_menu_items($menuID);
-  foreach ( $primaryNav as $navItem ) {
-  
-    $menu['title'] = $navItem->title;
-    $menu['url'] = $navItem->url;
-    $menu['ID'] = $navItem->ID;
-    array_push($menus, $menu);
-  
-  }
-  return $menus;
-}
-  
-add_action( 'rest_api_init', function () {
-  register_rest_route( 'menu/v1', '/header-menu', array(
-    'methods' => 'GET',
-    'callback' => 'get_header_menu_list',
+  $custom_post_type = get_posts( array(
+    'post_type' => 'products',
+    'posts_per_page' => -1,
+    'post_status' =>'publish',
+    'order'=> 'ASC'
   ) );
-} );
+  if ( empty( $custom_post_type ) ) {
+    return new WP_Error( '404', 'No custom post type found', array( 'status' => 404 ) );
+  }
+
+  $custom_post_type_data = array();
+  foreach ( $custom_post_type as $post ) {
+    $custom_post_type_data[] = array(
+      'id' => $post->ID,
+      'title' => $post->post_title,
+      'content' => apply_filters( 'the_content', $post->post_content ),
+    );
+  }
+
+  return new WP_REST_Response( $custom_post_type_data, 200 );
+}
+
 
 // add_action('init', 'init_test');
 function init_test(){
-$menuLocations = get_nav_menu_locations();
-$menuID = $menuLocations['primary'];
-$menus = [];
-
-$primaryNav = wp_get_nav_menu_items($menuID);
-foreach ( $primaryNav as $navItem ) {
-
-  $menu['title'] = $navItem->title;
-  $menu['url'] = $navItem->url;
-  array_push($menus, $menu);
-
-  print_r($navItem);
-}
-
- 
- 
 
 }
-
-
-
-
